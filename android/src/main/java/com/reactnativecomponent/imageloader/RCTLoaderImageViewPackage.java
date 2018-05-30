696d0f1b98542402f8b6f986624853a295084424
zhai yuanji <yuanji.zhai@mdland.com>
2018-05-30 17:40:38 +0800

    update package

--- android/src/main/java/com/reactnativecomponent/imageloader/RCTLoaderImageViewPackage.java
+++ android/src/main/java/com/reactnativecomponent/imageloader/RCTLoaderImageViewPackage.java
@@ -17,7 +17,7 @@ public class RCTLoaderImageViewPackage implements ReactPackage {
         return Collections.emptyList();
     }
 
-    @Override
+   // @Override // not need after 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
